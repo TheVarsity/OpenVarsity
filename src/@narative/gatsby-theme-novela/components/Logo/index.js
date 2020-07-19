8a0c82d8123b643a0ca4225354bd4bd38f1b188b
Rahul Tarak <rahultarak12345@gmail.com>
2020-07-19 18:25:04 +0530

    📱 Fixed Mobile Checking

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -13,7 +13,7 @@ export default function Logo({ fill }) {
     <svg
       version="1.0"
       xmlns="http://www.w3.org/2000/svg"
-      width={isMobile > 600 ? "60vh" : "25vh"}
+      width={!isMobile ? "60vh" : "25vh"}
       max-width="60vh"
       height="100%"
       viewBox="0 0 768.000000 113.000000"
